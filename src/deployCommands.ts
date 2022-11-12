607bb6fe829e7783a6114c0eb0001dd72bfac839
guilermmm <guilherme.r.a.melo@gmail.com>
2022-11-12 06:09:57 -0300

    test

--- src/deployCommands.ts
+++ src/deployCommands.ts
@@ -14,6 +14,8 @@ for (const module of Object.values<Command>(commandModules)) {
   commands.push(module.data)
 }
 
+//
+
 const rest = new REST({ version: '9' }).setToken(config.DISCORD_TOKEN)
 rest
   .put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), {
